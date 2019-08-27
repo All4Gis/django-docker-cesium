fb3193f3f292e725a0d7c5610ae58a3ff443cbd9
Fran <franka1986@gmail.com>
2019-08-27 19:51:00 +0200

    update cesium lib

--- sample/static/CesiumViewer.js
+++ sample/static/CesiumViewer.js
@@ -1,6 +1,11 @@
 // Mininal Viewer
+var imageryProviders = Cesium.createDefaultImageryProviderViewModels();
+var selectedImageryProviderIndex = 4;  // MapBox Street is 5th in the list.
+
 var viewer = new Cesium.Viewer('cesiumContainer', {
-	baseLayerPicker : true,
+	geocoder: false,
+    imageryProviderViewModels: imageryProviders,
+    selectedImageryProviderViewModel: imageryProviders[selectedImageryProviderIndex],
 	homeButton : false,
 	timeline : false,
 	infoBox : false,
@@ -13,3 +18,27 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
 	timeline : false
 });
 
+// Load GeoJson
+
+var geojson = "/static/data/sample.json"
+var promise = Cesium.GeoJsonDataSource.load(geojson);
+promise.then(function(dataSource) {
+    viewer.dataSources.add(dataSource);
+	var entities = dataSource.entities.values;
+	viewer.zoomTo(entities)
+    for (var i = 0; i < entities.length; i++) {
+        var entity = entities[i];
+        if (entity.properties.hasProperty('prop0')) {
+            var prop0 = entity.properties.prop0;
+            var prop0Value = prop0.valueOf();
+            if  (prop0Value.type ==='ellipse') {
+                entity.ellipse = new Cesium.EllipseGraphics({
+                    semiMinorAxis: prop0Value.semiMinorAxis,
+                    semiMajorAxis: prop0Value.semiMajorAxis,
+		    		height: prop0Value.height,
+                    material: Cesium.Color.fromCssColorString(prop0Value.color)
+                });
+            }
+        }
+    }
+});
